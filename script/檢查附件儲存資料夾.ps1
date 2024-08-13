32d8f2c825951d2c98e1f357cfe55d5a8463e9e8
William Chen <44458534+funwilliam@users.noreply.github.com>
2024-08-13 15:16:55 +0800

    update

--- script/檢查附件儲存資料夾.ps1
+++ script/檢查附件儲存資料夾.ps1
@@ -9,55 +9,6 @@ Import-Module -Name .\UserDefinedToolkit
 # 切換到專案根目錄
 Set-Location -Path $projectRootPath
 
-# 檢查虛擬環境是否已存在
-if (Test-Path -Path ".\venv") {
-    Write-Host "虛擬環境已存在，跳過建立步驟。"
-} else {
-    # 建立虛擬環境
-    Write-Host "建立虛擬環境..."
-    python -m venv venv
-}
-
-# 進入虛擬環境
-Write-Host "進入虛擬環境..."
-& .\venv\Scripts\Activate.ps1
-
-# 初始化專案的import
-Write-Host "初始化專案的import..."
-python .\app\helpers\auto_imports.py
-
-Pause
-
-# 安裝必要的套件
-Write-Host "安裝必要的套件..."
-pip install -r requirements.txt
-
-# 生成 SECRET_KEY 並寫入 .env 文件
-Write-Host "生成 SECRET_KEY 並寫入 .env 文件..."
-python .\app\helpers\generate_secret_key.py
-
-# 初始化資料庫
-Write-Host "初始化資料庫..."
-flask db init
-Write-Host "進行資料庫遷移..."
-flask db migrate -m "Initial migration."
-Write-Host "升級資料庫..."
-flask db upgrade
-
-Pause
-
-# 輸入資料到資料庫
-Write-Host "輸入資料到資料庫..."
-python seed_data.py
-
-# 確認 log 資料夾是否已建立
-if (Test-Path ".\log") {
-    Write-Output "log 資料夾已存在。"
-} else {
-    Write-Output "log 建立資料夾建立。"
-    New-Item -ItemType Directory -Path ".\log"
-}
-
 # 載入 .env 文件並取得環境變數
 $envFilePath = ".\.env"
 $baseFolder = ""
@@ -110,7 +61,5 @@ foreach ($subFolder in $subFolders) {
         Write-Output "資料夾 '$folder' 不存在。"
     }
 }
-
-Write-Host "所有操作已完成。"
 
 Pause
